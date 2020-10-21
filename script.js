1a63b947570fd8f99fc3b1f6c55aa492389c91ce
MrTops <43430084+MrTops@users.noreply.github.com>
2020-10-21 08:36:56 -0400

    fixed

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.onload( () => {
-    document.getElementsByClassName("whatAreYou")[0].setAttribute('title', new URLSearchParams(window.location.search).get('whatAreYou') || 'Nothing');
-});
+const whoAreYou = () => {
+    document.getElementsByClassName("what-are-you")[0].setAttribute('title', new URLSearchParams(window.location.search).get('whatAreYou') || 'Nothing');
+};
