42d0d612a0002ec436aec8afabae26556de2d388
MrTops <43430084+MrTops@users.noreply.github.com>
2020-10-21 08:21:33 -0400

    moved to onload

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.getElementsByClassName("whatAreYou")[0].setAttribute('title', new URLSearchParams(window.location.search).get('whatAreYou') || 'Nothing');
+document.onload( () => {
+    document.getElementsByClassName("whatAreYou")[0].setAttribute('title', new URLSearchParams(window.location.search).get('whatAreYou') || 'Nothing');
+});
